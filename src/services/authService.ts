81410dd7931971079fb11a6c6123984cfa88f015
woobigmac <woobigmac@gmail.com>
2026-05-14 16:43:49 +0900

    fix: request apple profile scopes

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -42,7 +42,7 @@ export async function loginWithApple(): Promise<AppleLoginResult> {
 
   const response = await appleAuth.performRequest({
     requestedOperation: appleAuth.Operation.LOGIN,
-    requestedScopes: [],
+    requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
     nonceEnabled: false,
   });
 
